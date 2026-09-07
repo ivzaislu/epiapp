b22b08c3bf11a1540faba27ce77c1f7ae3beccf3
ivzaislu <johann0789@googlemail.com>
2026-09-08 01:52:26 +0200

    Make Android APK server-agnostic

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,9 +11,8 @@ android {
         applicationId = "org.epiapp.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
-        buildConfigField("String", "BASE_URL", "\"https://epiapp.duckdns.org\"")
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures {
