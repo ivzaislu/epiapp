ca6ed04664dd6d16099decf0d97ad6a69ae98196
ivzaislu <johann0789@googlemail.com>
2026-09-09 22:47:07 +0200

    Bump Android version for branded launcher icon

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.epiapp.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     if (keystorePropertiesFile.exists()) {
