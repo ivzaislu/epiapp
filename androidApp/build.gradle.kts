75e7c82a98fe78be67014a435147718394106c7a
ivzaislu <johann0789@googlemail.com>
2026-09-19 07:50:54 +0200

    Prepare first signed Android release 0.3.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "org.epiapp.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.3.0"
     }
 
     if (releaseSigningRequested) {
